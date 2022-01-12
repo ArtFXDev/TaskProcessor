7a2dd59bd6125b0fb14443ac9f9369150c6c0623
SurajShettigar <sshettigar@artfx.fr>
2022-01-12 15:37:02 +0100

    Add json parser. Add action manager.
    - Added json parser for actions.
    - Added ActionData class. This class holds data being passed between actions (as input or output).
    - Added action manager to handle actions.
    - Added test function to test action manager.

--- src/pebbles/core/action_runtime.py
+++ src/pebbles/core/action_runtime.py
@@ -1,12 +1,11 @@
+import  src.pebbles.core as core
+
+
 class ActionRuntime(object):
 
     def __init__(self, definition):
         self.action_definition = definition
-        pass
 
     def execute(self):
-        print("Excecuting action: " + self.action_definition.label)
-        pass
-
-    def is_entity_supported(self, entity):
+        print("Executing action: " + self.action_definition.label)
         pass
